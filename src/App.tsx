12c4819bcb36598acf4528a3612c854a69ddcfdf
Max <max_erling@outlook.com>
2021-07-09 15:47:34 +0200

    setup chkara ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import "bulma/css/bulma.min.css";
 import { Header, Footer, Container } from "./components";
-
+import { VStack, Flex, Heading } from '@chakra-ui/react';
 function App() {
   return (
     <>
-      <Header></Header>
-      <Container></Container>
-      <Footer></Footer>
+    <VStack p={5}>
+      <Flex w='100%'>
+      <Heading ml='8' size='md' fontweight='semibold' color='cyan.400'>Ecom</Heading>
+      </Flex>
+      </VStack>
     </>
   );
 }
