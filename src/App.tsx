d44ed288b0da4391c63fefb1476c1e3d5ffc7daa
unknown <maximilianerling@gmail.com>
2021-07-10 07:09:54 +0200

    setup toggle light and dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import React from "react";
-import { Header, Footer, Container } from "./components";
-import { VStack, Flex, Heading } from '@chakra-ui/react';
+import {
+  VStack,
+  Flex,
+  Heading,
+  IconButton,
+  useColorMode,
+} from "@chakra-ui/react";
+import { FaMoon, FaSun, FaInstagram, FaGithub } from "react-icons/fa";
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark";
   return (
     <>
-    <VStack p={5}>
-      <Flex w='100%'>
-      <Heading ml='8' size='md' fontweight='semibold' color='cyan.400'>Ecom</Heading>
-      </Flex>
+      <VStack p={5}>
+        <Flex w="100%">
+          <Heading ml="8" size="md" fontweight="semibold" color="cyan.400">
+            Ecom
+          </Heading>
+        </Flex>
+        <IconButton
+          aria-label="social"
+          ml={8}
+          icon={isDark ? <FaSun /> : <FaMoon />}
+          isRound={true}
+          onClick={toggleColorMode}
+        ></IconButton>
       </VStack>
     </>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { ChakraProvider } from "@chakra-ui/react"
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import reportWebVitals from "./reportWebVitals";
+import { Header } from "./components/Header";
+import { Theme } from "./global/Theme";
 ReactDOM.render(
-       
+  
   <React.StrictMode>
     <ChakraProvider>
-    <App />
+      <ColorModeScript
+        initialColorMode={Theme.config.initialColorMode}
+      ></ColorModeScript>
+      <App />
     </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
